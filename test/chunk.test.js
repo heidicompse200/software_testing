7e08588c378784c869a43998187671b71d79c02c
Heidi Seppi <heidi.seppi@tuni.fi>
2024-12-07 11:25:47 +0200

    Added an example unit test

---
+++ test/chunk.test.js
@@ -0,0 +1,10 @@
+import { assert, expect } from 'chai';
+import Chunk from '../src/chunk.js';
+
+describe('Testing the Chunk Functions', function() {
+
+    it('Testi', function() {
+        var result = 5*2;
+        assert.equal(result, 10);
+    });
+});
